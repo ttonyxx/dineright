3040eb42ed88e50d8c64fe091f74acef4d20af97
Arjun Patrawala <arjunpatrawala@berkeley.edu>
2021-10-23 16:53:16 -0700

    someone died at ucsd

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,19 @@ app.post('/create-account', async (req, res) => {
   res.send(responses.success('Account created'));
 });
 
+app.post('/valid-user', async (req, res) => {
+  let { email, password } = req.body;
+  if (!email || !password) {
+    return res.send(responses.error('you suck and are a terrible human being'));
+  }
+
+  let r = await db.query('SELECT * FROM accounts WHERE email = $1 AND password = $2', [email, password]);
+  if (r.rows.length !== 0) {
+    return res.send(responses.success());
+  }
+  return res.send(responses.error());
+});
+
 
 app.listen(8080, () => {
   console.log('Server has started!');
